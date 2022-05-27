614638f161ab30ecc91e328cdc52f87dfaf49e29
DavidJCullen <87192838+DavidJCullen@users.noreply.github.com>
2022-05-27 21:17:35 +0100

    Update a1_final.py

--- PartA/a1_final.py
+++ PartA/a1_final.py
@@ -3,11 +3,11 @@ import statistics
 import time
 
 
-class PartA1(MRJob):#define the mapper.
+class PartA1(MRJob):
 	def mapper(self, _,line):
 		fields = line.split(',') # split by comma
-		try: # the transactions field has 7 fields. The below prevents malformed lines being inluded by requiring the lenght is equal to 7.
-			if len(fields) == 7: # if lenght of line is 7 run the code, otherwise pass on the exception (passes malformed lines)
+		try: 
+			if len(fields) == 7: # if length of line is 7 run the code, otherwise pass on the exception (passes malformed lines)
 				unix_timestamp = int(fields[6]) # access unix timestamp.
 				date = time.strftime("%m%y", time.gmtime(unix_timestamp)) # convert unix to gmtime.
 				yield ((date), 1) # yield date as key and 1 as value.
