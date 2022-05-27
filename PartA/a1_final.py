2c4465a22b244084a2a1c32514606903ee3b1b68
DavidJCullen <87192838+DavidJCullen@users.noreply.github.com>
2022-05-27 21:16:38 +0100

    Update a1_final.py

--- PartA/a1_final.py
+++ PartA/a1_final.py
@@ -3,20 +3,14 @@ import statistics
 import time
 
 
-
-"""
-Create a bar plot showing the number of transactions occurring every month between the start and end of the dataset. Create a bar plot showing the average value of transactions in each month between the start and end of the dataset. Note: As the dataset spans multiple years and you are aggregating together all transactions in the same month, make sure to include the year in your analysis.
-Note: Once the raw results have been processed within Hadoop/Spark you may create your bar plot in any software of your choice (excel, python, R, etc.)
-
-"""
 class PartA1(MRJob):#define the mapper.
 	def mapper(self, _,line):
 		fields = line.split(',') # split by comma
 		try: # the transactions field has 7 fields. The below prevents malformed lines being inluded by requiring the lenght is equal to 7.
-			if len(fields) == 7: # if lenght of line is 7 run the code otherwise pass on the exception (passes malformed lines)
+			if len(fields) == 7: # if lenght of line is 7 run the code, otherwise pass on the exception (passes malformed lines)
 				unix_timestamp = int(fields[6]) # access unix timestamp.
 				date = time.strftime("%m%y", time.gmtime(unix_timestamp)) # convert unix to gmtime.
-				yield ((date), 1) # yeild date as key and 1 as value.
+				yield ((date), 1) # yield date as key and 1 as value.
 		except:
 			pass
 
