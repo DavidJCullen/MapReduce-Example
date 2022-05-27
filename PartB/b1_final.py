5e210bd386d7a7a78018edd2f385c4ce073c6dfc
DavidJCullen <87192838+DavidJCullen@users.noreply.github.com>
2022-05-27 21:19:18 +0100

    Update b1_final.py

--- PartB/b1_final.py
+++ PartB/b1_final.py
@@ -3,24 +3,24 @@ from mrjob.job import MRJob
 import re
 
 class PartB1_Initial_Agregation(MRJob):
-    #define mapper
+  
 
    def mapper(self,__,line):
-       #split fields
+       
         fields = line.split(",")
         try:
             if len(fields) == 7:    #if field lenght ==7 continue, otherwise pass.
-                to_address = fields[2]
+                to_address = fields[2] 
                 value = float(fields[3])
                 yield(to_address,value)
         except:
             pass
 
-   def combiner(self,address,total): #use comobiner to speed up computation
-        yield (address, sum(total))# yeild address and the total value
+   def combiner(self,address,total): 
+        yield (address, sum(total))
 
-   def reducer(self,address,total):#send to reducer
-        yield (address, sum(total)) ##yeild address and the total value
+   def reducer(self,address,total):
+        yield (address, sum(total)) ##yield address and the total value
 
 if __name__ == '__main__':
 
